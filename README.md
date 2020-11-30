7213faf58c2600ce50d3b5251aa5202a686013d9
seungjoon <seungjoon.oh@kt.com>
2020-11-30 16:55:08 +0900

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# graphql-start
+# graphql-start를 위한 레포지토리 입니다.
+다음과 같이 GraphQL Server와 React + GraphQL 로 이루어져 있습니다.
+
+- hackernews-node: GraphQL 서버
---
+++ hackernews-node/src/index.js
@@ -0,0 +1,27 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { PrismaClient } = require('@prisma/client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Link
+}
+
+const prisma = new PrismaClient()
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: request => {
+    return {
+      ...request,
+      prisma,
+    }
+  },
+})
+server.start({ port: 5000 }, () => console.log(`서버가 작동중입니다.. http://localhost:5000`))
