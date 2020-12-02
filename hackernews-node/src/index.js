e13bec1841c0672954bc594821b48b00b06cb1b4
seungjoon <seungjoon.oh@kt.com>
2020-12-02 10:34:15 +0900

    add subscription, vote, filter, pagination

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,17 +1,23 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { PrismaClient } = require('@prisma/client')
+const { PubSub } = require('graphql-yoga')
 const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription')
+const Vote = require('./resolvers/Vote')
 
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
-  Link
+  Link,
+  Vote,
 }
 
+const pubsub = new PubSub()
 const prisma = new PrismaClient()
 
 const server = new GraphQLServer({
@@ -21,6 +27,7 @@ const server = new GraphQLServer({
     return {
       ...request,
       prisma,
+      pubsub
     }
   },
 })
